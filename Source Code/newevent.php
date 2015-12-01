6f517645eb0bf2870b7f3b4704f3ca07c144d86a
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-12-01 15:11:13 +0000

    Não quero perder esta merda :poop:

--- Source Code/newevent.php
+++ Source Code/newevent.php
@@ -16,8 +16,17 @@
 
 		include 'PHP/topnav.php';
 	?>
-	
-	<div class="EventForm">
+		<h3>Add New Event</h3>
+		<form action="Database/event_creation.php" method="post" enctype="multipart/form-data"> 
+			<label for="Event_Id">Nome:</label><input type="text" name="Event_Id" id="Event_Id"> <br>
+			<label for="Event_Description">Descrição:</label><textarea name="Event_Description" id="Event_Description">Inserir Descrição</textarea> <br> 
+			<label for="Event_Type">Tipo de evento:</label><input type="text" name="Event_Type" id="Event_Type"><br>
+			<label for="Event_Date">Data:</label><input type="date" name="Event_Date" id="Event_Date"><br>
+			<label for="Event_image">Imagem:</label><input type="file" name="Event_image" id="Event_image"><br>
+			
+			<input type="submit" name="Event_Submit"> </input>
+			
+		</form>
 
 	</div>
 </body>
