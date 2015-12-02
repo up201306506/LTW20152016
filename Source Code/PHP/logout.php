58177367e8a75acda5edc0b57b4ccc96fa93fffe
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-12-02 14:19:31 +0000

    Log Out mostra uma mensagem :mens:

--- Source Code/PHP/logout.php
+++ Source Code/PHP/logout.php
@@ -2,7 +2,9 @@
 
 session_start();
 if (session_destroy()) {
-	header('Location: ../index.php');
+	echo	'<span id="message">Logged out Successfully. Redirecting to Main Page...</span>';
+	
+	header("refresh:3; url=../index.php");
 }
 
 ?>
