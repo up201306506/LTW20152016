6ca2c1361885cbaa52c46b1d83ced41d07be0c88
JoaoIsaias <jl.isaias123@gmail.com>
2015-12-02 14:29:25 +0000

    Topnav melhorzinha (link no nome pro profile) :100:
    Izi

--- Source Code/PHP/topnav.php
+++ Source Code/PHP/topnav.php
@@ -3,9 +3,10 @@
 echo '<div class="navigation">';
 	echo '<img id="topnavimg" src="Images/logo2.png">';
 	echo '<div class="Information">';
-		echo '<span id="topnavusername">Username: ' . $_SESSION['login_user'] . '</span>';
-		echo '<a href="PHP/logout.php">Log Out</a>';
-		echo '<a href="PHP/settings.php">Settings</a>';
+		echo '<span> Username:</span>';
+		echo '<a class="topnavtext" href="profile.php">'. $_SESSION['login_user'] . '</a>';
+		echo '<a class="topnavtext" href="PHP/settings.php">Settings</a>';
+		echo '<a  class="topnavtext" href="PHP/logout.php">Log Out</a>';
 	echo '</div>';
 echo '</div>';
 echo '<div class="navSpacing"></div>';
