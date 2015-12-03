618e53eb086a7616454a73603ff722cae5cdc3e5
Mosaal <alexandremoreira_13@sapo.pt>
2015-12-03 11:20:01 +0000

    Topnav

--- Source Code/PHP/topnav.php
+++ Source Code/PHP/topnav.php
@@ -1,9 +1,7 @@
 <ul>
 	<a class="home_icon" href=""><img src="Images/home_icon.png"></a>
 	<input class="search_bar" type="text" placeholder="PESQUISA">
-	<div class="wrapper">
-		<li><a href="PHP/logout.php">Log Out</a></li>
-		<li><a href="settings.php">Settings</a></li>
-		<li><a href="profile.php">User: <?php echo $_SESSION['login_user']; ?></a></li>
-	</div>
+	<li><a href="PHP/logout.php">Log Out</a></li>
+	<li><a href="settings.php">Settings</a></li>
+	<li><a href="profile.php">User: <?php echo $_SESSION['login_user']; ?></a></li>
 </ul>
