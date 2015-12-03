a19cdaeaff9180c13a2d16abc41b4eafb94e16b8
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-12-03 14:39:13 +0000

    Erros :currency_exchange:
    oops

--- Source Code/PHP/signup.php
+++ Source Code/PHP/signup.php
@@ -4,11 +4,11 @@
 $error = '';
 
 if (isset($_POST['signup'])) {
-	if (empty($_POST['username']) || empty($_POST['password'])) {
+	if (empty($_POST['username_signup']) || empty($_POST['password_signup'])) {
 		$error = 'Username or Password is invalid!';
 	} else {
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+		$username = $_POST['username_signup'];
+		$password = $_POST['password_signup'];
 		insertIntoUser($username, $password);
 	}
 }
