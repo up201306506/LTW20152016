f4d527ec351c41ee8e4133d68aa3145ef8fec2eb
Mosaal <alexandremoreira_13@sapo.pt>
2015-12-04 11:57:22 +0000

    Profile stuff

---
+++ Source Code/PHP/event_box.php
@@ -0,0 +1,62 @@
+<?php
+
+function createdEventBox() {
+	$userevents = getEventsByUserID($_SESSION['user_id']);
+	$eventtypes = getAllEventTypes();
+				
+	if (!empty($userevents)) {	
+		foreach($userevents as $row) {
+			$creator = getUserByID($row['user_id']);
+			$link = '"event.php?id=' . $row['id'] . '"';
+
+			$alt = '"image_' . $row['event_id'] . '"';
+			$imgPath = '"Images/Event/' . $row['image_path'] . '.jpg"';
+
+			$value = '"' . $row['id'] . '"';
+			?>
+
+			<div class="event_box">
+				<a href=<?php echo $link; ?>><?php echo $row['event_id']; ?></a>
+				<span class="event_type"><?php echo $eventtypes[$row['type']]['type']; ?></span>
+				<span class="event_user">Created by:<?php echo $creator['user_name']; ?></span>
+				<span class="event_date"><?php echo $row['date']; ?></span>
+				<img class="event_img" src=<?php echo $imgPath; ?> alt=<?php echo $alt; ?>>
+				<form action="" method="post">
+					<button name="Delete_button" class="delete_button" value=<?php echo $value; ?>>Cancel Event</button>
+				</form>
+			</div>
+
+			<?php
+		}
+	}
+}
+
+function attendingEventBox() {
+	$userattendance = getEventsUserAttends($_SESSION['user_id']);
+	$eventtypes = getAllEventTypes();
+	
+	if (!empty($userattendance)) {
+		foreach($userattendance as $row) {
+			$creator = getUserByID($row['user_id']);
+			$link = '"event.php?id=' . $row['id'] . '"';
+
+			$alt = '"image_' . $row['event_id'] . '"';
+			$imgPath = '"Images/Event/' . $row['image_path'] . '.jpg"';
+
+			$value = '"' . $row['id'] . '"';
+			?>
+
+			<div class="event_box">
+				<a href=<?php echo $link; ?>><?php echo $row['event_id']; ?></a>
+				<span class="event_type"><?php echo $eventtypes[$row['type']]['type']; ?></span>
+				<span class="event_user">Created by:<?php echo $creator['user_name']; ?></span>
+				<span class="event_date"><?php echo $row['date']; ?></span>
+				<img class="event_img" src=<?php echo $imgPath; ?> alt=<?php echo $alt; ?>>
+			</div>
+
+			<?php
+		}
+	}
+}
+
+?>
--- Source Code/profile.php
+++ Source Code/profile.php
@@ -21,6 +21,7 @@
 		include 'Database/user_connection.php';
 		include 'PHP/topnav.php';
 		include 'PHP/event_deletion.php';
+		include 'PHP/event_box.php';
 	?>
 	<input class="add_event" Onclick="location.href = 'newevent.php';" type="button" value="Add Event" name="AddEventButton">
 	<div class="event-container">
@@ -28,52 +29,8 @@
 			<li class="tab-link current" data-tab="tab-1">Created Events</li>
 			<li class="tab-link" data-tab="tab-2">Attending Events</li>
 		</ul>
-		<div class="tab-content current" id="tab-1">
-			<?php
-				$userevents = getEventsByUserID($_SESSION['user_id']);
-				$eventtypes = getAllEventTypes();
-				
-				if (!empty($userevents))
-				{	
-					foreach($userevents as $row)
-					{
-						$creator = getUserByID($row['user_id']);
-						echo '<div class="eventBox">';
-							echo '<a href="event.php?id=' . $row['id'] . '"><span>' . $row['event_id'] . '<span></a><br>';
-							echo '<span class="EventType">' . $eventtypes[$row['type']]['type'] . '<span><br>';
-							echo '<span class="EventUser"> Created by: '. $creator['user_name'] . '<span><br>';
-							echo '<span class="EventDate">' . $row['date'] . '<span><br>';
-							echo '<img src="Images/Event/'.$row['image_path'].'.jpg" alt="image_'.$row['event_id'].'" class="event_image"/>';
-							echo '<form action="" method="post">';
-								echo "<button name='Delete_button' class='deletebutton' value='" . $row['id'] . "'>Delete Event</button>";
-							echo '</form>';
-						echo '</div>';
-					}
-				}
-			?>
-		</div>
-		<div class="tab-content" id="tab-2">
-			<?php	
-				$userattendance = getEventsUserAttends($_SESSION['user_id']);
-				if (!empty($userattendance))
-				{
-					foreach($userattendance as $row)
-					{
-						$creator = getUserByID($row['user_id']);
-						echo '<div class="eventBox">';
-							echo '<a href="event.php?id=' . $row['id'] . '"><span>' . $row['event_id'] . '<span></a><br>';
-							echo '<span class="EventType">' . $eventtypes[$row['type']]['type'] . '<span><br>';
-							echo '<span class="EventUser"> Created by: '. $creator['user_name'] . '<span><br>';
-							echo '<span class="EventDate">' . $row['date'] . '<span><br>';
-							echo '<img src="Images/Event/'.$row['image_path'].'.jpg" alt="image_'.$row['event_id'].'" class="event_image"/>';
-							echo '<form action="" method="post">';
-								echo "<button name='Delete_button' class='deletebutton' value='" . $row['id'] . "'>Delete Event</button>";
-							echo '</form>';
-						echo '</div>';
-					}	
-				}
-			?>
-		</div>
+		<div class="tab-content current" id="tab-1"><?php createdEventBox(); ?></div>
+		<div class="tab-content" id="tab-2"><?php attendingEventBox(); ?></div>
 	</div>
 </body>
 </html>
