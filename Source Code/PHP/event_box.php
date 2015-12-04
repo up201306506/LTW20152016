583ee287f022840ff1fb6ca4989ad0115402fcd0
Mosaal <alexandremoreira_13@sapo.pt>
2015-12-04 12:56:54 +0000

    LEL

--- Source Code/PHP/event_box.php
+++ Source Code/PHP/event_box.php
@@ -16,9 +16,9 @@ function createdEventBox() {
 			?>
 
 			<div class="event_box">
-				<a href=<?php echo $link; ?>><?php echo $row['event_id']; ?></a>
+				<a class="event_id" href=<?php echo $link; ?>><?php echo $row['event_id']; ?></a>
 				<span class="event_type"><?php echo $eventtypes[$row['type']]['type']; ?></span>
-				<span class="event_user">Created by:<?php echo $creator['user_name']; ?></span>
+				<span class="event_user">Created by: <?php echo $creator['user_name']; ?></span>
 				<span class="event_date"><?php echo $row['date']; ?></span>
 				<img class="event_img" src=<?php echo $imgPath; ?> alt=<?php echo $alt; ?>>
 				<form action="" method="post">
@@ -47,9 +47,9 @@ function attendingEventBox() {
 			?>
 
 			<div class="event_box">
-				<a href=<?php echo $link; ?>><?php echo $row['event_id']; ?></a>
+				<a class="event_id" href=<?php echo $link; ?>><?php echo $row['event_id']; ?></a>
 				<span class="event_type"><?php echo $eventtypes[$row['type']]['type']; ?></span>
-				<span class="event_user">Created by:<?php echo $creator['user_name']; ?></span>
+				<span class="event_user">Created by: <?php echo $creator['user_name']; ?></span>
 				<span class="event_date"><?php echo $row['date']; ?></span>
 				<img class="event_img" src=<?php echo $imgPath; ?> alt=<?php echo $alt; ?>>
 			</div>
