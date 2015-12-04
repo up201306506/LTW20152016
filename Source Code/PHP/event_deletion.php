84124bc9d8d80e5744377914baac4f4935d237ac
Mosaal <alexandremoreira_13@sapo.pt>
2015-12-04 15:35:50 +0000

    TopNav concertada

--- Source Code/PHP/event_deletion.php
+++ Source Code/PHP/event_deletion.php
@@ -5,7 +5,6 @@ if (isset($_POST['Delete_button'])) {
 	if (existsEventByID($event_to_delete))	
 	{
 		$event_name =  deleteEvent($event_to_delete);
-		echo '<span id="message">The Event has been deleted</span>';
 	}
 	
 }
--- Source Code/PHP/topnav.php
+++ Source Code/PHP/topnav.php
@@ -4,10 +4,9 @@
 	<li><a href="settings.php">Settings</a></li>
 	<li><a href="profile.php">User: <?php echo $_SESSION['login_user']; ?></a></li>
 	<li>
-		<form action="searchbar.php" method="get">
-			<input id="search_bar" name="search_bar" class="search_bar" type="text" placeholder="SEARCH">
-			<img class="lupa" src="Images/lupa.png" type="submit">
-			<input type="submit">
+		<form class="search_form" action="searchbar.php" method="get">
+			<input name="search_bar" class="search_bar" type="text" placeholder="SEARCH">
+			<button class="lupa" type="submit"></button>
 		</form>
 	</li>
 </ul>
