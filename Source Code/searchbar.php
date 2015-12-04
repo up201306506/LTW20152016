5365b03db6728360cc58e0f041689121def9eea9
JoaoIsaias <jl.isaias123@gmail.com>
2015-12-04 15:50:22 +0000

    search básico a dar para eventos e users :meh:

--- Source Code/searchbar.php
+++ Source Code/searchbar.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>Profile</title>
-	<!--<script src="JQuery/jquery-1.11.3.min.js"></script>
-	<script type="text/javascript" src="JQuery/settings.js"></script>-->
+	<script src="JQuery/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="JQuery/profile.js"></script>
 	<link rel="stylesheet" type="text/css" href="Styles/topnav.css">
 	<link rel="stylesheet" type="text/css" href="Styles/search.css">
 	<link href='https://fonts.googleapis.com/css?family=Jura:600' rel='stylesheet' type='text/css'>
@@ -19,9 +19,10 @@
 		include 'PHP/topnav.php';
 		include 'Database/user_connection.php';
 		include 'Database/event_connection.php';
+		include 'Database/event_type_connection.php';
 		include 'PHP/changeuser.php';
 		include 'PHP/changepass.php';
-
+		$search = $_GET['search_bar'];
 	?>
 	<div class="search-container">
 		<ul class="tabs">
@@ -30,21 +31,19 @@
 		</ul>
 		<div class="tab-content current" id="tab-1">
 			<?php
-				$search = $_GET['search_bar'];
-				echo 'Bar: ' . $search;
-				$events = getAllEvents();
-				$eventtypes = getAllEventTypes();
-				
-				if (!empty($events))
+				$allevents = getAllEvents();
+				$eventypes = getAllEventTypes();
+				if (!empty($allevents))
 				{	
-					foreach($events as $row)
+					foreach($allevents as $row)
 					{
-						if($events)
+						if(strpos(strtolower($row[event_id]),strtolower($search)) !== false)
 						{
+
 							$creator = getUserByID($row['user_id']);
-							echo '<div class="eventBox">';
+							echo '<div class="searchBox">';
 								echo '<a href="event.php?id=' . $row['id'] . '"><span>' . $row['event_id'] . '<span></a><br>';
-								echo '<span class="EventType">' . $eventtypes[$row['type']]['type'] . '<span><br>';
+								echo '<span class="EventType">' . $eventypes[$row['type']]['type'] . '<span><br>';
 								echo '<span class="EventUser"> Created by: '. $creator['user_name'] . '<span><br>';
 								echo '<span class="EventDate">' . $row['date'] . '<span><br>';
 								echo '<img src="Images/Event/'.$row['image_path'].'.jpg" alt="image_'.$row['event_id'].'" class="event_image"/>';
@@ -58,26 +57,19 @@
 		</div>
 		<div class="tab-content" id="tab-2">
 			<?php
-			/*	
-				$userattendance = getEventsUserAttends($_SESSION['user_id']);
-				if (!empty($userattendance))
+				$allusers = getAllUsers();
+				if (!empty($allusers))
 				{
-					foreach($userattendance as $row)
+					foreach($allusers as $row)
 					{
-						$creator = getUserByID($row['user_id']);
-						echo '<div class="eventBox">';
-							echo '<a href="event.php?id=' . $row['id'] . '"><span>' . $row['event_id'] . '<span></a><br>';
-							echo '<span class="EventType">' . $eventtypes[$row['type']]['type'] . '<span><br>';
-							echo '<span class="EventUser"> Created by: '. $creator['user_name'] . '<span><br>';
-							echo '<span class="EventDate">' . $row['date'] . '<span><br>';
-							echo '<img src="Images/Event/'.$row['image_path'].'.jpg" alt="image_'.$row['event_id'].'" class="event_image"/>';
-							echo '<form action="" method="post">';
-								echo "<button name='Delete_button' class='deletebutton' value='" . $row['id'] . "'>Delete Event</button>";
-							echo '</form>';
-						echo '</div>';
+						if(strpos(strtolower($row[user_name]),strtolower($search)) !== false)
+						{
+							echo '<div class="searchBox">';
+								echo '<span> Username: '. $row['user_name'] . '<span><br>';
+							echo '</div>';
+						}	
 					}	
 				}
-				*/
 			?>
 		</div>
 	</div>
