9c4a2e87574a899a593170c4ffa9aa412821743c
JoaoIsaias <jl.isaias123@gmail.com>
2015-12-05 17:52:56 +0000

    o alex pediu :no_good:

--- Source Code/PHP/signup.php
+++ Source Code/PHP/signup.php
@@ -13,6 +13,11 @@ if (isset($_POST['signup'])) {
 		$hashedpass = password_hash($password, PASSWORD_DEFAULT, $options);
 		
 		insertIntoUser($username, $hashedpass);
+		$user = getUserByUserName($username);
+
+		$_SESSION['login_user'] = $username;
+		$_SESSION['user_id'] = $user['id'];
+		echo '<script>window.location = "profile.php"</script>';
 	}
 }
 
